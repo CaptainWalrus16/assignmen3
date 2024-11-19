5421545ca0f116acb8ccc0fed1fcea2a681a226d
CaptainWalrus16 <131814203+CaptainWalrus16@users.noreply.github.com>
2024-11-19 10:24:18 -0500

    Add files via upload

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,24 @@
+class RulesSet():
+    def __init__(self,r):
+        self.rules = r
+        self.values = []
+        self.first = []
+        self.follow = []
+    
+    def buildValues(self):
+        for i in self.rules():
+            self.values.append(i)
+        for i in self.values():
+            
+    def First(self):
+        for j in self.values():
+            for i in self.rules():
+                if i[0:1].isalpha()  == False:
+               
+    def Follow(self):
+        self.follow = "woah"
+    
+
 file = open('grammar.txt','r')
 rules = file.readlines()
 points = []
@@ -5,14 +26,4 @@ for i in rules:
     entry = i.split()
     points.append(entry)
 mainrules = RulesSet(points)
-
-class RulesSet():
-    def __init__(self,r):
-        self.rules = r
-        self.first
-        self.follow
-    
-    def First(self):
-        
-    def Follow(self):
-        
+print(points)
